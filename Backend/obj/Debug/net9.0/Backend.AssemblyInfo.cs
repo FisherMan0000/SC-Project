30b19398ae767b046b55bb3331a3d9b63fa7e0f2
Wathana <wattana.waipat@gmail.com>
2025-01-24 15:30:38 +0700

    Hello Test Hiring

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00000a6f48bf5a37d4aa8de0cef33b1e42453624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee397e9762e9d2acde7959fbd0d248f526e4ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
