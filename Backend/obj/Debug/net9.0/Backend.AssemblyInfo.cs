312131404ce84543e4b5d0574805cb88361ead9c
Wathana <wattana.waipat@gmail.com>
2025-01-27 19:57:39 +0700

    adjust addManger api

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b19398ae767b046b55bb3331a3d9b63fa7e0f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d93da79bb5eecac631123d54bcbcba11c5cbc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
